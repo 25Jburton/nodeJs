de8c9a340259370e86b62cfead2f6e75721915b1
25Jburton <25jburton@gmail.com>
2025-04-01 12:38:52 -0400

    Basic example of a node server setup

---
+++ nodeServer.js
@@ -0,0 +1,21 @@
+// Simple web server using Node HTTP package
+
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Create HTTP server
+const server = http.createServer(function (req, res) {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send the response body "Page Content"
+  res.end("Page Content\n");
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function () {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
